90f8757062f665599a85b856bcaa90ed0a1a0c99
office3rock <105702924+office3rock@users.noreply.github.com>
2024-01-12 14:10:47 +0200

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -18,26 +18,26 @@ module.exports = {
   subscribeWidget: {
     visible: false,
     title: "popis2013",
-    helpText: "הישארו מעודכנים",
-    successMessage: "תודה ההודעה נשלחה בהצלחה",
+    helpText: "Keep following",
+    successMessage: "Thank you The message was sent successfully",
   },
   header: {
     navigation: [
       {
-        label: "דף הבית",
+        label: "Homepage",
         url: "https://popis2013.net/",
       },
       {
-        label: "יצירת קשר",
+        label: "Contact",
         url: "https://popis2013.net/contact",
       },
     ],
   },
   footer: {
-    copyright: "כל הזכויות שמורות ",
+    copyright: "All rights reserved",
     navigation: [
       {
-        label: "דף הבית",
+        label: "Homepage",
         url: "https://popis2013.net/",
       },
       {
@@ -46,7 +46,7 @@ module.exports = {
       },
       
       {
-        label: "יצירת קשר",
+        label: "Contact",
         url: "https://popis2013.net/contact",
       },
       
